39b0168f397f4daf20cec172e750e407399e5f87
chsantosh <22pa1a0529@vishnu.edu.in>
2024-09-25 15:38:17 +0530

    issued successfully

--- conn.py
+++ conn.py
@@ -1,5 +1,6 @@
 from supabase import create_client, Client
 
-url: str = "https://clvvpgmntfgznsahgrar.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsdnZwZ21udGZnem5zYWhncmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzNzgzNzcsImV4cCI6MjA0MDk1NDM3N30.mLbfZ0gWMQaQEsswfHkgJ17p7fnYhk0mWYsDJkuf5Qs"
+url: str = "https://jhtxzmjribqxtapgvulw.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpodHh6bWpyaWJxeHRhcGd2dWx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzNjU3MjgsImV4cCI6MjA0MDk0MTcyOH0.dYB3t2ZLtblz7aTpV9tmmtD1rAzqcYFFcIcL64qyR3Q"
+
 supabase: Client = create_client(url, key)
